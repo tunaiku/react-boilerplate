3eb2580caaaacdd07312e1ebdb743c31b496a508
ishak imanuel <ishakimanuellmp@gmail.com>
2019-03-30 20:58:26 +0700

    serve next generation image format

--- configs/index.js
+++ configs/index.js
@@ -1,32 +1,55 @@
 // import server dependencies
 
-const express = require('express');
-const compression = require('compression');
-const morgan = require('morgan');
-const path = require('path');
-const loadable = require('react-loadable');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+const loadable = require("react-loadable");
+const cookieParser = require("cookie-parser");
+const mobileDetect = require("mobile-detect");
 
 // compile our react and esnext code
-require('./bootstrapper.js');
+require("./bootstrapper.js");
 
 // Our loader - this basically acts as the entry point for each page load
-const loader = require('./loader.js');
+const loader = require("./loader.js");
 
 // Create our express app using the port optionally specified
 const app = express();
 const PORT = process.env.PORT || 3001;
+const prodEnv = "production";
 
 // Compress, parse, log, and raid the cookie jar
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
 app.use(cookieParser());
 
+// set default header
+app.use((req, res, next) => {
+  res.header("Access-Control-Max-Age", "3600");
+  res.header("Cache-Control", "public, max-age=2592000");
+  res.header("Expires", new Date(Date.now() + 2592000000).toUTCString());
+
+  return next();
+});
+
+// serve new generation images
+if (process.env.NODE_ENV === prodEnv) {
+  app.get(["*.jpg", "*.jpeg", "*.png"], (req, res, next) => {
+    const mb = new mobileDetect(req.headers["user-agent"]);
+    const browser = mb.userAgent();
+    if (browser === "Chrome" || browser === "Opera") {
+      const newImgUrl = req.url.replace(/\.(jpg|jpeg|png)\b/, ".webp");
+      req.url = newImgUrl;
+      res.set("Content-Type", "image/webp");
+    }
+    next();
+  });
+}
+
 // Set up homepage, static assets, and capture everything else
-app.use(express.Router().get('/', loader));
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.Router().get("/", loader));
+app.use(express.static(path.resolve(__dirname, "../build")));
 app.use(loader);
 
 // We tell React Loadable to load all required assets and start listening - ROCK AND ROLL!
@@ -35,19 +58,19 @@ loadable.preloadAll().then(() => {
 });
 
 // Handle the bugs somehow
-app.on('error', error => {
-  if (error.syscall !== 'listen') {
+app.on("error", error => {
+  if (error.syscall !== "listen") {
     throw error;
   }
-  const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT;
+  const bind = typeof PORT === "string" ? "Pipe " + PORT : "Port " + PORT;
 
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
